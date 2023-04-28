004367bc61d448ffddefe4511439a6e568e3269a
Devfrank94 <francescomurro94@gmail.com>
2023-04-28 16:04:01 +0200

    partial commit

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,9 +1,11 @@
 <script>
+import Comics from '../data/dc-comics.json';
+
 export default {
   name:'Main',
   data(){
     return {
-    
+    Comics,
     }
   },
 }
@@ -17,10 +19,18 @@ export default {
   </div>
   <body>
 
-    <div id="currseries">current series</div>
+    
+    <div class="container d-flex">
+      
+      <div id="currseries">current series</div>
 
-    <div class="container d-flex al-item-cent">
-
+      <div
+        class="card al-item-cent"
+        v-for="(comic, index) in Comics"
+        :key="index">
+        <img :src="comic.thumb">
+        <span>{{comic.series}}</span>
+      </div>
   
     
   
@@ -45,23 +55,44 @@ export default {
 
   body{
     background-color: $fmblack;
-    height: 600px;
+    
+  }
+
+  .container{
+
     position: relative;
+    flex-wrap: wrap;
+
+    .card{
+      width: calc(100% / 6);
+      margin: 1.5rem 0;
+      padding: 0 10px;
+      color: $fmwhite;
+
+      img{
+          width: 100%;
+          aspect-ratio: 1 / 1;
+          margin-bottom: .4rem;
+      }
+
+    }    
 
     #currseries{
       position: absolute;
       top: -25px;
-      left: 490px;
+      left: 0;
       background-color: $fmcyan;
       text-transform: uppercase;
       color: $fmwhite;
       font-weight: 600;
-      font-size: 1.5rem;
+      font-size: 1.2rem;
       padding: .6rem;
     }
+    
 
   }
 
+  
   h2{
     color: white;
   }
